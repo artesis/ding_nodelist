cca0597073ed3017543c11fe20fb7ed28def6abd
Alex Damaschin <ad@peytz.dk>
2018-04-10 17:31:51 +0300

    DBC-1397 - Adjust Nodelist displays.

--- templates/ding_nodelist.ding_news.single.tpl.php
+++ templates/ding_nodelist.ding_news.single.tpl.php
@@ -13,18 +13,12 @@
  */
 
 $image = _ding_nodelist_get_dams_image_info($item, 'field_list_image');
-$edbase = field_view_field('node', $item, 'field_editorial_base_n', 'teaser');
 ?>
 <div class="item">
   <div class="article_image">
     <?php print $image ? theme('image_style', array_merge($image, array('style_name' => $conf['image_style']))) : ''; ?>
   </div>
   <div class="article-info">
-    <div class="category"><?php print drupal_render($edbase);?>
-      <span class="date-created">
-        <?php print _ding_nodelist_get_ding_news_date($item); ?>
-      </span>
-    </div>
     <h3><a href="<?php print url('node/' . $item->nid);?>"><?php print $item->title;?></a></h3>
     <div class="node">
       <?php
